e2c0b7ea502fe8bfac67cc6a61c97e77c1119d5b
yhao <yghaosh@gmail.com>
2016-02-25 14:17:02 +0800

    Write getName and getDate. Write a simple Task object.

--- QuickTasker/src/model/Task.java
+++ QuickTasker/src/model/Task.java
@@ -1,5 +1,25 @@
 package model;
 
-public class Task {
+import java.util.Date;
 
+public class Task {
+    private String _taskName;
+    private Date _deadline;
+    
+    public Task() {
+        
+    }
+    
+    public Task(String taskName, Date deadline) {
+        _taskName = taskName;
+        _deadline = deadline;
+    }
+    
+    private String getName() {
+        return _taskName;
+    }
+    
+    private Date getDate() {
+        return _deadline;
+    }
 }
