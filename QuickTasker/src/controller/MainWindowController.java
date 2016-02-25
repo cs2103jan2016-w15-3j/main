f3a1c7fb3441693bcdb5a4a6f67401ecdc2099f9
kenan <xinkenan.11@hotmail.com>
2016-02-25 12:46:11 +0800

    add mainWindow prototype

--- QuickTasker/src/controller/MainWindowController.java
+++ QuickTasker/src/controller/MainWindowController.java
@@ -1,10 +1,26 @@
 package controller;
 
+import com.jfoenix.controls.JFXTextField;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
 public class MainWindowController {
+    // Views
+    @FXML Label label;
+    @FXML JFXTextField commandBox;
     
     private Main main;
     
     public void setMain(Main main){
         this.main =main;
+    }
+    
+    public void handleEnterKeyPressed(KeyEvent event){
+        if (event.getCode().equals(KeyCode.ENTER)){
+           
+        }
     }
 }
