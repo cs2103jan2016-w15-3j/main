0c0229af1a6e5cc34c3c2494280d3379c0924315
Dawson <freezedry24@gmail.com>
2016-02-26 15:30:26 +0800

    "added commands.java"

---
+++ QuickTasker/src/controller/Commands.java
@@ -0,0 +1,14 @@
+package controller;
+/**
+ * 
+ * author A0121558H
+ *         Lau Dawson
+ *
+ **/
+public enum Commands {
+    
+
+      CREATE_TASK, UPDATE_TASK, DISPLAY_TASK, DELETE_TASK;  
+     
+
+}
