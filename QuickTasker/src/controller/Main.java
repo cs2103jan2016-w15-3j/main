bd4d446687b347be5b4736d5d360f467417893bf
jlin95 <leeway95@gmail.com>
2016-02-26 16:30:25 +0800

    edit Main

--- QuickTasker/src/controller/Main.java
+++ QuickTasker/src/controller/Main.java
@@ -5,7 +5,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.text.Font;
 import javafx.scene.paint.Color;
 import javafx.scene.layout.AnchorPane;
