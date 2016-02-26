be5ce1eb7b2cc02fba5444f6654f59eddeb7c7a2
yhao <yghaosh@gmail.com>
2016-02-26 17:26:17 +0800

    "add in AddTask and Command"

---
+++ QuickTasker/src/logic/AddTask.java
@@ -0,0 +1,21 @@
+package logic;
+
+import java.util.List;
+
+import model.Task;
+
+public class AddTask implements Command {
+
+    @Override
+    public void execute(List<Task> list, Task task) {
+        executeAdd(list, task);
+    }
+    
+    private void executeAdd(List<Task> list, Task task) {
+        if (task != null) {
+            list.add(task);
+            
+        }
+    }
+    
+}
---
+++ QuickTasker/src/logic/Command.java
@@ -0,0 +1,12 @@
+package logic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import controller.InputInterface;
+import model.Task;
+
+public interface Command {
+    void execute(List<Task> list, String details);
+}
+
