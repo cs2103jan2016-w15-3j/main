0e21a8b8be1a2c29982ea10366a58cf35999bfda
yhao <yghaosh@gmail.com>
2016-02-27 09:46:05 +0800

    format Command.java

--- QuickTasker/src/logic/Command.java
+++ QuickTasker/src/logic/Command.java
@@ -1,12 +1,9 @@
 package logic;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import controller.InputInterface;
 import model.Task;
 
 public interface Command {
     void execute(List<Task> list, Task task);
 }
-
