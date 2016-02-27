1d0733538d7d86c90f25d5a4c4ff2d0e66125f9f
yhao <yghaosh@gmail.com>
2016-02-27 09:47:03 +0800

    Add commandMap into ProcessInput

--- QuickTasker/src/logic/ProcessInput.java
+++ QuickTasker/src/logic/ProcessInput.java
@@ -2,25 +2,32 @@ package logic;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.TreeMap;
+
+import javax.activation.CommandMap;
+import javax.lang.model.type.PrimitiveType;
+
+import com.sun.javafx.collections.MappingChange.Map;
 
 import controller.Commands;
 import controller.InputInterface;
 import controller.InputParser;
 import model.Task;
 
-public class ProcessInput implements Command {
+public class ProcessInput {
+    private TreeMap<Commands, Command> commandMap;
+
     public ProcessInput(String userInput, List<Task> tasks) {
         InputParser parser = new InputParser();
-        Commands command = parser.outputCommand(userInput);
+        String command = parser.outputCommand(userInput);
         String name = parser.outputTaskName(userInput);
         LocalDate endDate = parser.outputDate(userInput);
-        Task task = new Task(name, endDate);
-        command.execute(tasks, new Task());
+        Task task = new Task(name, LocalDate.now(), endDate);
+        commandMap.get(command).execute(tasks, task);
     }
-    
-    @Override
-    public void execute(List<Task> list, Task task) {
-        // TODO Auto-generated method stub
-        
+
+    public void populateCommandMap() {
+        commandMap = new TreeMap<Commands, Command>();
+        commandMap.put(Commands.CREATE_TASK, new AddTask());
     }
 }
