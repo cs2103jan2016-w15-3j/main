588e414426558de34b4da6aaaf390c94aa3ca5d3
dev-seahouse <xinkenan.11@hotmail.com>
2016-03-01 01:31:36 +0800

    add pseudo code for add task, deletetask, update task

--- QuickTasker/src/controller/MainWindowController.java
+++ QuickTasker/src/controller/MainWindowController.java
@@ -32,9 +32,9 @@ public class MainWindowController implements Initializable {
      */
     @FXML Label label;
     @FXML JFXTextField commandBox;
-    
+
     @FXML JFXListView<Task> taskListView;
-    ObservableList<Task> list = FXCollections.observableArrayList();
+    ObservableList<Task> observableList = FXCollections.observableArrayList();
 
     public void setMain(Main main) {
         this.main = main;
@@ -46,11 +46,47 @@ public class MainWindowController implements Initializable {
         if (event.getCode().equals(KeyCode.ENTER)) {
             LocalDate startDate = LocalDate.now();
             LocalDate deadLine = LocalDate.now();
-            list.add(new Task("RandomTask", startDate, deadLine));
-            taskListView.setItems(list);
+            String taskDescription = commandBox.getText(); 
+            observableList.add(new Task(taskDescription, startDate, deadLine));
+            taskListView.setItems(observableList);
             commandBox.clear();
         }
     }
+    
+    private void getCommandType(String commandString) {
+        //InputParser parser = new Parser();
+            // string commandType = parser.determineCommand(commandString); 
+        // if command is "add"
+          // private void addTask(){
+          // String startDate = parser.getStartDate(commandString);
+          // String deadLine = parser.getDeadLine(commandString);
+          // String taskDetail = parser.getTaskDetail(commandString);
+          // Task task = new Task(taskDetail, startDate, deadline);
+          // logic.addTask(task); // main logic class add task into internal list and 
+          // this.observableList = logic.getTasks() // returns list of task can be linkedlist, arraylist, map etc. anything
+          // taskListview.setItems(observableList); // refresh ui
+        //}
+        
+        // if command is "delete"
+          // private void deleteTask(int num){
+          //  int taskTodelete = parser.getTaskToDelete(String commandString):
+          //  Task task = observableList.get(taskToDelete - 1); 
+          //  logic.deleteTask(task) //think of what parameter is needed to locate a task in a list for deletion?
+          //  this.observableList = logic.getTasks();
+          //  taskListView.setItesm(observableList); refresh ui
+        // if command is "update"
+         // private void updateTask(){
+         // similar to addTask
+         // }
+        // if command is "undo"
+         // private void undo(){
+          //  //this feature requires 
+          //  logic.undoPrevious()// logic calls  
+          //
+        
+        
+    }
+    
 
     private void setCellFactory() {
         taskListView.setCellFactory(param -> new TaskListCell());
