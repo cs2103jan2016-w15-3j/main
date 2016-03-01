f1d5f83d14d4948cfd650e7f3c1fff36c98adb5e
yhao <yghaosh@gmail.com>
2016-03-01 15:38:33 +0800

    Modify AddTask to take in generic types

--- QuickTasker/src/logic/AddTask.java
+++ QuickTasker/src/logic/AddTask.java
@@ -1,16 +1,22 @@
 package logic;
 
 import java.util.List;
+/*
+*
+* author A0130949
+* Soh Yonghao 
+* 
+**/
 
 import model.Task;
 
-public class AddTask implements Command {
-
+public class AddTask<E> implements Command {
+    
     @Override
-    public void execute(List<Task> list, Task task) {
-        executeAdd(list, task);
+    public void execute(List list, Object task) {
+        executeAdd(list, (Task) task); 
     }
-
+    
     private void executeAdd(List<Task> list, Task task) {
         if (task != null) {
             list.add(task);
