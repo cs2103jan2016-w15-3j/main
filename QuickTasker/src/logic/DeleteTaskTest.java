4c85864a0cee7855744eedc2c5145646fd4bf7ef
yhao <yghaosh@gmail.com>
2016-03-01 15:37:18 +0800

    Add test for delete function

---
+++ QuickTasker/src/logic/DeleteTaskTest.java
@@ -0,0 +1,34 @@
+package logic;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import model.Task;
+
+public class DeleteTaskTest {
+    Logic logic;
+    @Before
+    public void setUp() throws Exception {
+        logic = new Logic();
+        for(int i = 0; i < 10; i++) {
+            Task task = new Task(i);
+            logic.list.add(task);
+        }
+    }
+
+    @Test
+    public void test() throws Exception {
+        setUp();
+        assertEquals(logic.getSize(), 10);
+        logic.deleteTask(0);
+        assertEquals(logic.getSize(), 9);
+        logic.deleteTask(8);
+        assertEquals(logic.getSize(), 8);
+        
+    }
+
+}
