ca70fa022d485b2af721bf93157058f09abe0c4e
yhao <yghaosh@gmail.com>
2016-03-01 15:39:57 +0800

    Comment out the ProcessInput but not deleted in case we need to reference it

--- QuickTasker/src/logic/ProcessInput.java
+++ QuickTasker/src/logic/ProcessInput.java
@@ -1,18 +1,17 @@
-package logic;
+/*package logic;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.TreeMap;
-
-import javax.activation.CommandMap;
-import javax.lang.model.type.PrimitiveType;
-
-import com.sun.javafx.collections.MappingChange.Map;
-
 import controller.Commands;
-import controller.InputInterface;
 import controller.InputParser;
 import model.Task;
+
+ *
+ * author A0130949
+ * Soh Yonghao 
+ * 
+ *
 
 public class ProcessInput {
     private TreeMap<Commands, Command> commandMap;
@@ -20,9 +19,11 @@ public class ProcessInput {
     public ProcessInput(String userInput, List<Task> tasks) {
         InputParser parser = new InputParser();
         populateCommandMap();
-/*        String command = parser.outputCommand(userInput);
-        String name = parser.outputTaskName(userInput);
-        LocalDate endDate = parser.outputDate(userInput);*/
+        
+         * String command = parser.outputCommand(userInput); String name =
+         * parser.outputTaskName(userInput); LocalDate endDate =
+         * parser.outputDate(userInput);
+         
         Task task = new Task("name", LocalDate.now(), LocalDate.now());
         commandMap.get(Commands.CREATE_TASK).execute(tasks, task);
     }
@@ -31,4 +32,4 @@ public class ProcessInput {
         commandMap = new TreeMap<Commands, Command>();
         commandMap.put(Commands.CREATE_TASK, new AddTask());
     }
-}
+}*/
