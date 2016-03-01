451012212ce1ed883656a083d45cf6753fd422d1
yhao <yghaosh@gmail.com>
2016-03-01 15:41:11 +0800

    Add a parameter index for Task object

--- QuickTasker/src/model/Task.java
+++ QuickTasker/src/model/Task.java
@@ -6,33 +6,45 @@ public class Task {
     private String _taskName;
     private static LocalDate _deadline;
     private LocalDate _startDate;
+    private static int _index = 0;
     
     public void setStartDate(LocalDate startDate) {
         _startDate = LocalDate.now();
     }
-    public Task() {        
-
+    
+    public Task(int index) {        
+        _index = index;
         _taskName = null;
         _deadline = null;
         _startDate = null;
     }
     
-    public Task(String taskName, LocalDate deadline) {
+    public Task(String taskName, LocalDate deadline, int index) {
         _taskName = taskName;
         _deadline = deadline;
+        _index = index;
     }
     
-    public Task(String taskName, LocalDate deadline, LocalDate startDate) {
+    public Task(String taskName, LocalDate deadline, LocalDate startDate, int index) {
         _taskName = taskName;
         _deadline = deadline;
         _startDate = startDate;
+        _index = index;
+    }
+    
+    public int getIndex() {
+        return _index;
+    }
+    
+    public void setIndex(int newIndex) {
+        _index = newIndex;
     }
 
     public String getName() {
         return _taskName;
     }
 
-    public LocalDate getDueDate() {
+    public LocalDate getDate() {
         return _deadline;
     }
     
@@ -41,11 +53,11 @@ public class Task {
         return _startDate;
     }
     
-    private void setName(String newName) {
+    public void setName(String newName) {
         _taskName = newName;
     }
 
-    private static void setDate(int year, int month, int date) {
+    public static void setDate(int year, int month, int date) {
         LocalDate newDate = LocalDate.of(date, month, year);
         _deadline = newDate;
     }
