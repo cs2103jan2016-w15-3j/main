a1f313522b23b8c47ed3f24482b73de96af0d733
kenan <xinkenan.11@hotmail.com>
2016-03-01 23:14:55 +0800

    [JsonTaskAccessTest.java] add setup() initializer

--- QuickTasker/tests/dao/JsonTaskDataAccessTest.java
+++ QuickTasker/tests/dao/JsonTaskDataAccessTest.java
@@ -1,11 +1,25 @@
 package dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Before;
+import org.junit.Test;
+
+import model.Task;;
 
 public class JsonTaskDataAccessTest {
+    List<Task> plannerBook;
+    TaskDataAccessObject dataHandler;
 
     @Before
     public void setUp() throws Exception {
+        plannerBook = new ArrayList<Task>();
+        dataHandler = new JsonTaskDataAccess();
+    }
+
+    @Test
+    public void testConvertOneTaskObjToJson() {
 
     }
 
