9eb5c09d703faeaba59759d2a50f26b8d2879359
jlin95 <leeway95@gmail.com>
2016-03-02 10:04:57 +0800

    Add boolean method for checking empty input

--- QuickTasker/src/controller/MainWindowController.java
+++ QuickTasker/src/controller/MainWindowController.java
@@ -1,8 +1,10 @@
 package controller;
 
+import java.awt.color.ICC_ColorSpace;
 import java.net.URL;
 import java.time.LocalDate;
 import java.util.ResourceBundle;
+import java.util.PrimitiveIterator.OfDouble;
 
 import com.jfoenix.controls.JFXListView;
 import com.jfoenix.controls.JFXTextField;
@@ -14,6 +16,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import jdk.internal.org.objectweb.asm.commons.StaticInitMerger;
 import model.Task;
 import view.TaskListCell;
 
@@ -44,18 +47,30 @@ public class MainWindowController implements Initializable {
     
     private void handleEnterKeyPressed(KeyEvent event) {
         if (event.getCode().equals(KeyCode.ENTER)) {
+            if (!isEmptyInput(commandBox.getText())) {
             LocalDate startDate = LocalDate.now();
             LocalDate deadLine = LocalDate.now();
             list.add(new Task(commandBox.getText(), startDate, deadLine));
             taskListView.setItems(list);
             commandBox.clear();
+            String commandStrin = commandBox.getText();
+            }
         }
+        
+    }
+    
+    /*
+     *Author:  A0133333U
+     * When input is empty and enter key is hit, no empty cell is created
+     */
+    private boolean isEmptyInput(String input){
+        if(input == null || input.isEmpty() || input.trim().equals("")) {
+            return true;
+        }
+        return false;
     }
 
-    private void parseCommand(String string) {
-
-    }
-
+    
     private void setCellFactory() {
         taskListView.setCellFactory(param -> new TaskListCell());
     }
