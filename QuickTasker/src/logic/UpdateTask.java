5b369940d07f0cb29f70093eb571c5b87a8fd412
yhao <yghaosh@gmail.com>
2016-03-07 10:32:16 +0800

    Update method added

---
+++ QuickTasker/src/logic/UpdateTask.java
@@ -0,0 +1,13 @@
+package logic;
+
+import java.util.List;
+
+public class UpdateTask<E> implements Command {
+
+    @Override
+    public void execute(List list, Object task) {
+        
+        
+    }
+    
+}
