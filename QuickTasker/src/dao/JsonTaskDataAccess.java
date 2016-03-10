6011c768cdfe513931aab0f1bc95f3913277179e
kenan <xinkenan.11@hotmail.com>
2016-03-11 02:56:28 +0800

    add save and read tasks

--- QuickTasker/src/dao/JsonTaskDataAccess.java
+++ QuickTasker/src/dao/JsonTaskDataAccess.java
@@ -1,27 +1,82 @@
 package dao;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
 import model.Task;
 
-public class JsonTaskDataAccess implements TaskDataAccessInterface {
+public class JsonTaskDataAccess implements TaskDataAccessObject {
+    private Path pathOfSaveFile;
+    private SettingManager settings = new SettingManager();
 
-    @Override
-    public List<Task> getTasks() {
-        // TODO Auto-generated method stub
-        return null;
+    public JsonTaskDataAccess() {
+        initialize();
+    }
+
+    private void initialize() {
+        this.pathOfSaveFile = Paths.get(settings.getPathOfSaveFile());
+        if (Files.notExists(pathOfSaveFile)) {
+            createNewSaveFile();
+        }
+    }
+
+    private void createNewSaveFile() throws CreateSaveFileException {
+        try {
+            Files.createFile(pathOfSaveFile);
+        } catch (IOException e) {
+            throw new CreateSaveFileException();
+        }
+
     }
 
     @Override
-    public void save(Task task) {
-        // TODO Auto-generated method stub
-
+    public List<Task> getTasks() throws GetTasksException {
+        try {
+            BufferedReader reader = Files.newBufferedReader(pathOfSaveFile);
+            Gson gson = new Gson();
+            List<Task> tasks = gson.fromJson(reader, new TypeToken<List<Task>>() {}.getType());
+            reader.close();
+            return tasks;
+        } catch (IOException e) {
+            throw new GetTasksException();
+        }
     }
 
     @Override
-    public void save(List<Task> tasks) {
-        // TODO Auto-generated method stub
+    public void save(Task task) throws SaveTasksException {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String json = gson.toJson(task);
+        try {
+            BufferedWriter writer = Files.newBufferedWriter(pathOfSaveFile);
+            writer.write(json);
+            writer.close();
 
+        } catch (IOException e) {
+            throw new SaveTasksException();
+        }
+    }
+
+    @Override
+    public void save(List<Task> tasks) throws SaveTasksException {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String json = gson.toJson(tasks);
+        try {
+            BufferedWriter writer = Files.newBufferedWriter(pathOfSaveFile);
+            writer.write(json);
+            writer.close();
+
+        } catch (IOException e) {
+            throw new SaveTasksException();
+        }
     }
 
     @Override
@@ -30,4 +85,14 @@ public class JsonTaskDataAccess implements TaskDataAccessInterface {
 
     }
 
+    public Path getFilePath() {
+        // TODO Auto-generated method stub
+        return this.pathOfSaveFile;
+    }
+
+    public static class CreateSaveFileException extends RuntimeException {};
+
+    public static class GetTasksException extends RuntimeException {};
+
+    public static class SaveTasksException extends RuntimeException {};
 }
--- QuickTasker/src/model/Task.java
+++ QuickTasker/src/model/Task.java
@@ -75,4 +75,34 @@ public class Task {
         this.id = IdGenerator++;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((endDate == null) ? 0 : endDate.hashCode());
+        result = prime * result + id;
+        result = prime * result + ((startDate == null) ? 0 : startDate.hashCode());
+        result = prime * result + ((taskName == null) ? 0 : taskName.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+        Task other = (Task) obj;
+        if (endDate == null) {
+            if (other.endDate != null) return false;
+        } else if (!endDate.equals(other.endDate)) return false;
+        if (id != other.id) return false;
+        if (startDate == null) {
+            if (other.startDate != null) return false;
+        } else if (!startDate.equals(other.startDate)) return false;
+        if (taskName == null) {
+            if (other.taskName != null) return false;
+        } else if (!taskName.equals(other.taskName)) return false;
+        return true;
+    }
+
 }
