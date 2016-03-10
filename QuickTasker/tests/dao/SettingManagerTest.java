856b3dc5d7d41ada32c19eb140007922b3d624c5
kenan <xinkenan.11@hotmail.com>
2016-03-11 02:52:43 +0800

    add ability to store and read settings

---
+++ QuickTasker/tests/dao/SettingManagerTest.java
@@ -0,0 +1,47 @@
+package dao;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
+import org.apache.commons.configuration2.Configuration;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SettingManagerTest {
+    SettingManager settingManager;
+    File file = new File("settings.properties");
+
+    @Before
+    public void setUp() throws Exception {
+        settingManager = new SettingManager();
+    }
+
+    @Test
+    public void settingsFileShouldAlwaysExist() {
+        assertTrue(file.exists());
+    }
+
+    @Test
+    public void ifSettingsFileIsEmptyShouldSetDefault() {
+        Configuration settings = null;
+        removeEverythingFromSettingsFile(settings);
+
+        settingManager = new SettingManager();
+        settings = settingManager.getConfigs();
+        assertFalse(settings.isEmpty());
+    }
+
+    // if critical properties does not exist (accidentally removed) , should be set default
+    // eg.saveFilePath = save.json
+
+    // more thorough approach : look for customSettings.properties first, if critical ones does not
+    // exist, look for the default.properties
+
+    private void removeEverythingFromSettingsFile(Configuration settings) {
+        settings = settingManager.getConfigs();
+        settings.clear();
+    }
+
+}
