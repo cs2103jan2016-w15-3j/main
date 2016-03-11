db5949487e43b8b7ce19ec9d433d9d256d559dda
yhao <yghaosh@gmail.com>
2016-03-11 16:55:05 +0800

    integrate with parser for UpdateTask

--- QuickTasker/src/logic/UpdateTask.java
+++ QuickTasker/src/logic/UpdateTask.java
@@ -4,21 +4,26 @@ import java.util.List;
 
 import model.Task;
 import parser.UserInputParser;
-/*
+
 public class UpdateTask<E> implements Command {
 
     @Override
     public void execute(List list, Object userInput) {
-        UserInputParser parser = new UserInputParser();
-        String updates = (String) userInput;
-        Task newTask = new Task(parser.getTaskName(updates), parser.getStartDate(updates), parser.getEndDate(updates));
-        executeUpdate(parser.getIndexForUpdate(taskIndex), newTask, list);
+        try {
+            UserInputParser parser = new UserInputParser();
+            String updates = (String) userInput;
+            Task newTask = new Task(parser.getTaskNameForUpdate(updates), parser.getStartDateForUpdate(updates),
+                    parser.getEndDateForUpdate(updates));
+            executeUpdate(parser.getIndexForUpdate(updates), newTask, list);
+        } catch (Exception e) {
+             System.out.println("aaa");
+        }
     }
     
     
     public void executeUpdate(int taskIndex, Task newTask, List<Task> list) {
+        System.out.println(" aaaa   "  + newTask.getName());
         list.set(taskIndex, newTask);
     }
 
 }
-*/
