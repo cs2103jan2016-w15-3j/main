97e5f6cb90bb325f272578e3e2a15ddd666feb83
yhao <yghaosh@gmail.com>
2016-03-11 15:43:58 +0800

    add UpdateTask class

--- QuickTasker/src/logic/UpdateTask.java
+++ QuickTasker/src/logic/UpdateTask.java
@@ -2,11 +2,23 @@ package logic;
 
 import java.util.List;
 
+import model.Task;
+import parser.UserInputParser;
+/*
 public class UpdateTask<E> implements Command {
 
     @Override
-    public void execute(List list, Object task) {
-
+    public void execute(List list, Object userInput) {
+        UserInputParser parser = new UserInputParser();
+        String updates = (String) userInput;
+        Task newTask = new Task(parser.getTaskName(updates), parser.getStartDate(updates), parser.getEndDate(updates));
+        executeUpdate(parser.getIndexForUpdate(taskIndex), newTask, list);
+    }
+    
+    
+    public void executeUpdate(int taskIndex, Task newTask, List<Task> list) {
+        list.set(taskIndex, newTask);
     }
 
 }
+*/
