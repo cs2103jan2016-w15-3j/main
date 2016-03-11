0c7718a684d512ac81ef226ccf1329a7c0b4f38a
Dawson <freezedry24@gmail.com>
2016-03-11 12:57:03 +0800

    Added flexible commands to DetermineCommandType.java

--- QuickTasker/src/parser/DetermineCommandType.java
+++ QuickTasker/src/parser/DetermineCommandType.java
@@ -5,16 +5,22 @@ public class DetermineCommandType {
     public static Commands getCommand(String input) {
 
         switch (input) {
-        case "add":
-            return Commands.CREATE_TASK;
-        case "delete":
-            return Commands.DELETE_TASK;
-        case "update":
-            return Commands.UPDATE_TASK;
-        case "read":
-            return Commands.DISPLAY_TASK;
-        default:
-            return Commands.ERROR;
+            case "add":
+                return Commands.CREATE_TASK;
+            case "del":
+                return Commands.DELETE_TASK;
+            case "remove":
+                return Commands.DELETE_TASK;
+            case "rm":
+                return Commands.DELETE_TASK;
+            case "delete":
+                return Commands.DELETE_TASK;
+            case "update":
+                return Commands.UPDATE_TASK;
+            case "read":
+                return Commands.DISPLAY_TASK;
+            default:
+                return Commands.ERROR;
         }
     }
 
