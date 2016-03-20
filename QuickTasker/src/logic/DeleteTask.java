775f84c5c38deae7927126ba884e940791c37bc4
yhao <yghaosh@gmail.com>
2016-03-20 23:59:58 +0800

    DeleteTask can redo now

--- QuickTasker/src/logic/DeleteTask.java
+++ QuickTasker/src/logic/DeleteTask.java
@@ -15,12 +15,12 @@ import model.Task;
 
 public class DeleteTask<E> implements Command<Object> {
     private Stack<Task> undoStack = new Stack<Task>();
+    private Stack<Task> redoStack = new Stack<Task>();
     
     @Override
     public void execute(List<Task> list, Object task) {
         undoStack.push((Task) list.get((int) task));
         executeDelete(list, (int) task);
-
     }
 
     private void executeDelete(List<Task> list, int index) {
@@ -29,7 +29,27 @@ public class DeleteTask<E> implements Command<Object> {
 
     @Override
     public void undo(ArrayList<Task> list) {
-        list.add(undoStack.pop());
-        
+        Task deletedTask = undoStack.pop();
+        redoStack.push(deletedTask);
+        list.add(deletedTask);
+    }
+
+    @Override
+    public void redo(ArrayList<Task> list) {
+        // TODO Auto-generated method stub
+        Task redoTask = redoStack.pop();
+        undoStack.push(redoTask);
+        int index = findTask(redoTask.getId(), list);
+        list.remove(index);
+    }
+    
+    private int findTask(int index, ArrayList<Task> list) {
+        int position = -1;
+        for (int i = 0 ; i < list.size(); i++) {
+            if (list.get(i).getId() == index) {
+                position = i;
+            } 
+        }
+        return position;
     }
 }
