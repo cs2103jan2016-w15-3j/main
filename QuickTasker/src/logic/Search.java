fc2dee99cf3f9da2f2542663ffd4e80ebb032010
yhao <yghaosh@gmail.com>
2016-03-20 15:31:37 +0800

    start working on search command

---
+++ QuickTasker/src/logic/Search.java
@@ -0,0 +1,20 @@
+package logic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Task;
+
+public class Search<E> implements Command<Object>{
+
+    @Override
+    public void execute(List<Task> list, Object task) {
+        
+    }
+
+    @Override
+    public void undo(ArrayList<Task> list) {
+        // TODO Auto-generated method stub
+        
+    }
+}
