d1560806143d5f98a345c93f14cc634a0b2e7ce2
yhao <yghaosh@gmail.com>
2016-03-20 22:14:29 +0800

    add all methods for update in

--- QuickTasker/src/parser/ParserInterface.java
+++ QuickTasker/src/parser/ParserInterface.java
@@ -18,6 +18,18 @@ public interface ParserInterface {
 
     String getTaskName(String userInput) throws Exception;
 
+    String setTaskNameForUpdates() throws Exception;
+
+    int getIndexForUpdate(String userInput);
+
+    String getTaskNameForUpdate(String userInput) throws Exception;
+
+    LocalDate getStartDateForUpdate(String userInput) throws Exception;
+
+    LocalDate getEndDateForUpdate(String userInput) throws Exception;
+
+    void setAttributesForUpdates(String input) throws Exception;
+
     int getTaskIndex(String userInput) throws Exception;
 
 }
