f5aac28345979aa9549a24a33f56c29b66c6e637
yhao <yghaosh@gmail.com>
2016-03-20 23:56:18 +0800

    AddTask can redo now

--- QuickTasker/src/logic/AddTask.java
+++ QuickTasker/src/logic/AddTask.java
@@ -18,6 +18,7 @@ public class AddTask<E> implements Command<Object> {
     private static final String NOTHING_TO_ADD = "Cannot add nothing";
     private static Logger loggerAdd = Logger.getLogger("log");
     private Stack<Task> undoStack = new Stack<Task>();
+    private Stack<Task> redoStack = new Stack<Task>();
 
     @Override
     public void execute(List<Task> list, Object task) {
@@ -44,7 +45,9 @@ public class AddTask<E> implements Command<Object> {
     
     @Override
     public void undo(ArrayList<Task> list) {
-        int index = findTask(undoStack.pop().getId(), (ArrayList<Task>) list);
+        Task undoTask = undoStack.pop();
+        redoStack.push(undoTask);
+        int index = findTask(undoTask.getId(), (ArrayList<Task>) list);
         if (index >= 0) {
             list.remove(index);
         } else {
@@ -60,5 +63,12 @@ public class AddTask<E> implements Command<Object> {
             } 
         }
         return position;
+    }
+
+    @Override
+    public void redo(ArrayList<Task> list) {
+        Task redoTask = redoStack.pop();
+        undoStack.push(redoTask);
+        list.add(redoTask);
     }
 }
