7e7ea92eab35ea80976b064e20345762643c0805
yhao <yghaosh@gmail.com>
2016-03-23 19:59:28 +0800

    Add more test for UpdateTask

--- QuickTasker/src/logic/UpdateTaskTest.java
+++ QuickTasker/src/logic/UpdateTaskTest.java
@@ -21,13 +21,38 @@ public class UpdateTaskTest {
         logic.addTask(new Task());
     }
 
-    @Test public void testUpdatingWithName() throws Exception {
+    @Test
+    // This is the regular update
+    public void testUpdatingWithName() throws Exception {
         logic.updateTask(new Task("name", LocalDate.now(), LocalDate.now()), 0);
         assertEquals("name", logic.getList().get(0).getName());
     }
 
-    @Test public void testUpdatingWithLongerName() throws Exception {
+    @Test
+    // A user could have updated a longer name
+    public void testUpdatingWithLongerName() throws Exception {
         logic.updateTask(new Task("name longer by alot", LocalDate.now(), LocalDate.now()), 1);
+        assertEquals("name longer by alot", logic.getList().get(1).getName());
+    }
+    
+    @Test
+    // A user could have changed the task to floating 
+    public void testUpdatingWithFloating() throws Exception {
+        logic.updateTask(new Task("name longer by alot", null, null), 1);
+        assertEquals("name longer by alot", logic.getList().get(1).getName());
+    }
+    
+    @Test
+    // A user could have changed name without start date
+    public void testUpdatingWithoutStartDate() throws Exception {
+        logic.updateTask(new Task("name longer by alot", null, LocalDate.now()), 1);
+        assertEquals("name longer by alot", logic.getList().get(1).getName());
+    }
+    
+    @Test
+    // A user could have changed name without due date
+    public void testUpdatingWithoutDueDate() throws Exception {
+        logic.updateTask(new Task("name longer by alot", LocalDate.now(), null), 1);
         assertEquals("name longer by alot", logic.getList().get(1).getName());
     }
 }
