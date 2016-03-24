672a1357a7bec9ea96344d56ce53eb8be514e759
yhao <yghaosh@gmail.com>
2016-03-25 01:53:16 +0800

    add AddRecurTask command

---
+++ QuickTasker/src/logic/AddRecurTask.java
@@ -0,0 +1,50 @@
+package logic;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.RecurringTask;
+import model.Task;
+
+public class AddRecurTask<RecurringTask> implements Command {
+    private static final String NOTHING_TO_ADD = "Cannot add nothing";
+    private static Logger loggerAdd = Logger.getLogger("log");
+    private Stack<Task> undoStack = new Stack<Task>();
+    private Stack<Task> redoStack = new Stack<Task>();
+    
+    @Override
+    public void execute(List list, Object op) {
+        assert (list.size() >= 0);
+        undoStack.push((model.RecurringTask) op);
+        executeAdd(list, (model.RecurringTask) op); 
+    }
+    
+    private void executeAdd(List<Task> list, model.RecurringTask op) {
+        loggerAdd.log(Level.INFO, "ADD RECUR TASK");
+        try {
+            if (op != null) {
+                list.add(op);
+            } else {
+                System.out.println(NOTHING_TO_ADD);
+            }
+        } catch (Exception e) {
+            loggerAdd.log(Level.WARNING, "ERROR", e);
+        }
+        loggerAdd.log(Level.INFO, "END");
+    }
+    
+    @Override
+    public void undo(ArrayList list) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void redo(ArrayList list) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
