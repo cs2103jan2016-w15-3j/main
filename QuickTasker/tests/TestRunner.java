79ce79afa0b46369a19973d2175406c5b4b48dda
kenan <xinkenan.11@hotmail.com>
2016-03-26 22:05:16 +0800

    moved all tests into right place

---
+++ QuickTasker/tests/TestRunner.java
@@ -0,0 +1,12 @@
+import data.JsonTaskDataAccessTest;
+import data.SettingManagerTest;
+import org.junit.runner.JUnitCore;
+
+public class TestRunner {
+    public static void main(String[] args) {
+        JUnitCore.runClasses(
+                JsonTaskDataAccessTest.class,
+                SettingManagerTest.class);
+
+    }
+}
