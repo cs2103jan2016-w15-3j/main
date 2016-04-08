66869087ffbfa697638d4ec29926bbb594f739e1
yhao <yghaosh@gmail.com>
2016-04-08 18:38:52 +0800

    add in test for skip

---
+++ QuickTasker/tests/logic/SkipRecurTaskTest.java
@@ -0,0 +1,70 @@
+package logic;
+
+import model.RecurringTask;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.Assert.assertEquals;
+
+public class SkipRecurTaskTest {
+    Logic logic;
+    @Before
+    public void setUp() throws Exception {
+        logic = new Logic();
+        logic.clear();
+        createRecurringTasks(3);
+    }
+
+    private void createRecurringTasks(int n) {
+        for (int i = 0; i < n; i++) {
+            logic.addTask(new RecurringTask("Recurring " + i, LocalDate.now().plusDays(i), LocalDate.now().plusDays(i), "weeks", null, null, 2));
+        }
+    }
+
+    @Test
+    public void testSizeOfListAfterAdd() throws Exception {
+        assertEquals(logic.getSize(), 3);
+    }
+
+    @Test
+    public void testSkip() {
+        logic.skip(0);
+        assertEquals("Recurring 2", logic.getTasks().get(0).getName());
+        assertEquals(LocalDate.now().plusDays(2).plusWeeks(2), logic.getTasks().get(0).getStartDate());
+    }
+
+    @Test
+    public void testSkipNegativeNumber() {
+        boolean testNegative = false;
+        try {
+            logic.skip(-1);
+        } catch(IndexOutOfBoundsException e) {
+            testNegative = true;
+        }
+        assert (testNegative);
+    }
+
+    @Test
+    public void testSkipOverBound() {
+        boolean testOutOfBound = false;
+        try {
+            logic.skip(5);
+        } catch(IndexOutOfBoundsException e) {
+            testOutOfBound = true;
+        }
+        assert (testOutOfBound);
+    }
+
+    @Test
+    public void testSkipNonNumberIndex() {
+        boolean testNonNumberIndex = false;
+        try {
+            logic.skip(Integer.parseInt("hi"));
+        } catch(NumberFormatException e) {
+            testNonNumberIndex = true;
+        }
+        assert (testNonNumberIndex);
+    }
+}
