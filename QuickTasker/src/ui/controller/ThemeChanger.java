2f7304c2c926ef579f067b2034b9d8d8f60fec4d
jlin95 <leeway95@gmail.com>
2016-04-11 00:49:55 +0800

    clean up

--- QuickTasker/src/ui/controller/ThemeChanger.java
+++ QuickTasker/src/ui/controller/ThemeChanger.java
@@ -12,7 +12,10 @@ public class ThemeChanger {
 
     public void change(String userinput, Scene s) {
         String colorName = userinput.substring(userinput.indexOf(" ") + 1);
-        if (!ApplicationColor.hasColor(colorName)) return;
+        if (!ApplicationColor.hasColor(colorName)) {
+        	return;
+        }
+        
         ApplicationColor selectedColor = ApplicationColor.valueOf(colorName.toUpperCase());
         JFXDecorator decorator = (JFXDecorator) s.lookup(".jfx-decorator");
         JFXToolbar toolbar = (JFXToolbar) s.lookup(".jfx-tool-bar");
